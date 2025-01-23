69192faf263d6c57ebe334314fc0b9b3bd124450
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2025-01-23 21:38:33 +0300

    feature: add customLinkedList implementation

--- src/HistoryManager/InMemoryHistoryManager.java
+++ src/HistoryManager/InMemoryHistoryManager.java
@@ -1,42 +1,90 @@
 package HistoryManager;
 
 
-import task.Epic;
-import task.SubTask;
 import task.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final List<Task> tasks;
+    Map<Integer, Node> tasks;
+    private Node head;
+    private Node tail;
 
     public InMemoryHistoryManager() {
-        this.tasks = new ArrayList<>();
+        this.tasks = new HashMap<>();
     }
 
-    @Override
-    public List<Task> getHistory() {
-        return tasks;
+    public List<Task> getTasks() {
+        List<Task> items = new ArrayList<>();
+        Node current = head;
+        while (current != null) {
+            items.add(current.value);
+            current = current.after;
+        }
+        return items;
     }
 
     @Override
     public void add(Task task) {
-        if (tasks.size() >= 10) {
-            tasks.removeFirst();
+        if (task == null) {
+            return;
         }
-        if (task instanceof Epic) {
-            Epic epic = new Epic(task.getName(), task.getDescription());
-            epic.setId(task.getId());
-            tasks.add(epic);
-        } else if (task instanceof SubTask) {
-            SubTask subTask = new SubTask(task.getName(), task.getStatus(), task.getDescription(),
-                    ((SubTask) task).getEpicId());
-            subTask.setId(task.getId());
-            tasks.add(subTask);
+        if (tasks.containsKey(task.getId())) {
+            Node oldNode = tasks.get(task.getId());
+            removeNode(oldNode);
+        }
+        Node newNode = new Node(task);
+        tasks.put(task.getId(), newNode);
+        linkNodeAtEnd(newNode);
+    }
+
+    @Override
+    public void remove(int id) {
+        if (tasks.containsKey(id)) {
+            Node oldNode = tasks.get(id);
+            removeNode(oldNode);
+            tasks.remove(id);
+        }
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return getTasks();
+    }
+
+    private void linkNodeAtEnd(Node newTail) {
+        Node oldTail = tail;
+        tail = newTail;
+        if (oldTail == null)
+            head = newTail;
+        else {
+            newTail.before = oldTail;
+            oldTail.after = newTail;
+        }
+    }
+
+    private void removeNode(Node node) {
+        if (node == null) {
+            return;
+        }
+        Node before = node.before;
+        Node after = node.after;
+        if (before == null && after == null) {
+            head = null;
+            tail = null;
+        } else if (before == null) {
+            head = node.after;
+            head.before = null;
+        } else if (after == null) {
+            tail.after = null;
+            tail = node.before;
         } else {
-            tasks.add(new Task(task.getName(), task.getStatus(), task.getDescription()));
+            before.after = after;
+            after.before = before;
         }
     }
 }
---
+++ src/HistoryManager/Node.java
@@ -0,0 +1,17 @@
+package HistoryManager;
+
+
+import task.Task;
+
+public class Node {
+
+    public Task value;
+    public Node after;
+    public Node before;
+
+    public Node(Task task) {
+        this.value = task;
+        this.after = null;
+        this.before = null;
+    }
+}
